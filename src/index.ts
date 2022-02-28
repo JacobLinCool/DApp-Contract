1589a77057c06b3d0dbe45b07b1c8c3421e4c3ee
JacobLinCool <jacoblincool@gmail.com>
2022-03-01 03:26:19 +0800

    docs: add docstrings

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
 import { ethers } from "ethers";
 
+/**
+ * The status of a contract.
+ */
 export enum ContractStatus {
+    /** The methods do not require a signature are ready to be called. */
     Initialized,
+    /** All contract methods are ready to be called. */
     Connected,
 }
 
+/**
+ * A class that represents a contract.
+ */
 export class Contract {
+    /** The status of the Contract instance. */
     public status: ContractStatus = ContractStatus.Initialized;
+    /**
+     * The ethers contract instance.
+     * Created with provider before calling `connect()`,
+     * and re-created with signer after calling `connect()`.
+     */
     public contract: ethers.Contract | null = null;
+    /**  The injected ethereum object */
     public ethereum = window.ethereum || null;
+    /** The provider used to connect to the ethereum network before calling `connect()`. */
     public provider: ethers.providers.BaseProvider;
 
+    /**
+     *
+     * @param address The address of the contract.
+     * @param network The network id of the network that contract is deployed on.
+     * @param abi The ABI of the contract.
+     * @param provider The provider used to connect to the ethereum network before calling `connect()`.
+     */
     constructor(
         public address: string,
         public network: number,
@@ -21,6 +44,9 @@ export class Contract {
         this.contract = new ethers.Contract(this.address, this.abi, this.provider);
     }
 
+    /**
+     * Connect to Metamask, and replace the contract instance with the signer if successful.
+     */
     public async connect(): Promise<void> {
         if (this.status >= ContractStatus.Connected) {
             return;
